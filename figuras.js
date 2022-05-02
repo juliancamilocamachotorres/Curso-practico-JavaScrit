a5b28c17d4362105700529b4e6db5547ffce22cc
juliancamilocamachotorres <juliancamilo3099@hotmail.es>
2022-05-02 16:42:03 -0500

    Interaccion JS con html

--- figuras.js
+++ figuras.js
@@ -66,3 +66,20 @@ function areaCirculo(radio){
 
 
 console.groupEnd();
+
+
+//funciones para html
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
